ac43b58f664ae6e794b3e27adabba07dd9223fd8
yahya wijaya <yahyawijaya082@gmail.com>
2024-01-17 09:06:45 +0700

    feat : add new scheduler for get all orders and send in queue

--- src/main/java/com/phincon/microservice/crm/service/OrderServiceImpl.java
+++ src/main/java/com/phincon/microservice/crm/service/OrderServiceImpl.java
@@ -5,19 +5,27 @@ import com.phincon.microservice.crm.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.Message;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @Slf4j
 public class OrderServiceImpl implements OrderService {
-    private final OrderRepository orderRepository;
 
     @Autowired
-    public OrderServiceImpl(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
-    }
+    OrderRepository orderRepository;
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    private final Set<String> sentOrders = new HashSet<>();
 
 
 
@@ -27,15 +35,33 @@ public class OrderServiceImpl implements OrderService {
         createOrder(orderGet.getPayload()).subscribe();
     }
 
+    @Transactional
     @Override
     public Mono<Orders> createOrder(Orders order) {
-        Orders orderNew = new Orders();
-        orderNew.setId(order.getId());
-        orderNew.setProductId(order.getProductId());
-        orderNew.setStatus(order.getStatus());
-        orderNew.setPrice(order.getPrice());
-        Mono<Orders> save = orderRepository.save(orderNew);
-        log.info("Order saved: " + save);
-        return save;
+        try {
+            order.setStatus("Completed");
+            Mono<Orders> save = orderRepository.save(order);
+            jmsTemplate.convertAndSend("queue.status", "Success");
+            log.info("Order saved: " + save);
+            return save;
+        }catch (Exception e) {
+            log.info("Order failed: " + e.getMessage());
+            jmsTemplate.convertAndSend("queue.status", "Failed");
+            jmsTemplate.convertAndSend("queue.crm.register", order);
+            return Mono.just(order);
+        }
+
+    }
+
+    @Scheduled(cron = "*/10 * * * * ?")
+    public void getOrders() {
+        Flux<Orders> ordersFlux = orderRepository.findAll();
+        ordersFlux.filter(order -> !sentOrders.contains(order.getId()))
+                .doOnNext(order -> {
+                    log.info("Sending order " + order.getId());
+                    jmsTemplate.convertAndSend("queue.all.order", order);
+                    sentOrders.add(order.getId());
+                })
+                .subscribe();
     }
 }
